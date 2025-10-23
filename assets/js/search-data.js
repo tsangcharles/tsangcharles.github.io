ff5683a8c9ec97d1c769cd6a8ce26f751f73f074
tsangcharles <ck2tsang@gmail.com>
2025-10-23 03:30:29 +0000

    Deploying to gh-pages from @ tsangcharles/tsangcharles.github.io@38a4f7b5255fd49306d78e1a7fe48d7305dff6f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -145,6 +145,13 @@ ninja.data = [{
           window.open("https://instagram.com/tsangcharles", "_blank");
         },
       },{
+        id: 'social-wechat_qr',
+        title: 'Wechat_qr',
+        section: 'Socials',
+        handler: () => {
+          window.open("", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
